eb096200e115a6e34512d5e280022f67515589f9
Surya <Suryabaddika9@gmail.com>
2025-05-24 22:28:06 +0530

    Selenium

---
+++ Selenium/MouseClickHoldRelease.py
@@ -0,0 +1,20 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html")
+
+
+oslo = driver.find_element(By.ID,"box1")
+norway = driver.find_element(By.ID,"box101")
+
+actions = ActionChains(driver)
+actions.click_and_hold(oslo).move_to_element(norway).release().perform()
+
+time.sleep(4)
+driver.quit()
+
