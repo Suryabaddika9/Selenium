2bb79abbe4fb92c2aa01f8433a4875bc3d463e1d
Surya <Suryabaddika9@gmail.com>
2025-05-29 02:53:12 +0530

    Selenium

---
+++ Selenium/TakingScreenshotOfPageSection.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://omayo.blogspot.com/")
+
+Section = driver.find_element(By.ID,"LinkList1")
+
+Section.screenshot("Compendium.png")
