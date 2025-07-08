01b9f08540239d8d54229b5f39d12be883ca0f2e
Surya <Suryabaddika9@gmail.com>
2025-07-08 15:49:08 +0530

    Selenium

---
+++ Selenium/ExplicitWait.py
@@ -0,0 +1,20 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://omayo.blogspot.com/")
+
+driver.find_element(By.CLASS_NAME,"dropbtn").click()
+
+wait = WebDriverWait(driver,30)
+flipkart_option = wait.until(expected_conditions.visibility_of_element_located((By.LINK_TEXT,"Flipkart")))
+
+flipkart_option.click()
+
+time.sleep(4)
+driver.quit()
