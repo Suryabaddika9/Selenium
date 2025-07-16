6cd62983aa99a088cf5c19b32984c0502e55634d
Surya <Suryabaddika9@gmail.com>
2025-07-16 21:45:15 +0530

    Selenium

---
+++ PyTest/test_sample.py
@@ -0,0 +1,8 @@
+def test_sample_one():
+    print("Inside Test Sample One")
+
+def test_sample_two():
+    print("Inside Test Sample Two")
+
+def test_sample_three():
+    print("Inside Test Sample Three")
